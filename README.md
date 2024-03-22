800ae4f0c4fcc006dac00fc9ec910e3ee43284cb
sselecirPyM <1151047603@qq.com>
2024-03-22 17:03:54 +0800

    add source

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # bmp2video
 watch filesystem changes, write into ffmpeg, delete source immediately.
+
+观察文件系统改动，把文件写入FFmpeg，然后立即删除源文件。
+
+这个脚本试图为不支持FFmpeg的剪辑软件提供FFmpeg导出。
+
+# 使用
+确保系统上安装了FFmpeg
+
+安装依赖项：
+```
+pip install watchdog
+```
+
+运行：
+```
+python bmp2video.py
+```
+
+导出bmp序列到脚本所在的文件夹。
+
+在导出视频之后，导出aac音频到脚本所在的文件夹。脚本会合并输出。
+
+文件路径中最好不要有中文，否则会产生一些错误，一部分图片会被忽略。
+
+ffmpeg不能立即处理整个视频。请在任务管理器中将你的剪辑软件进程优先级设置为低，以免过快的产生图像。
+
+任务管理器->详细信息->选中进程->右键->设置优先级->低
---
+++ bmp2video.py
@@ -0,0 +1,77 @@
+import subprocess
+import os
+
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.video_file_name = 'videoonly.mp4'
+        self.file_name = 'output.mp4'
+        self.ffmpeg_process = None
+        self.frame_rate = 60
+        
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        src_path = event.src_path
+        if(src_path.endswith("0000.bmp") and self.ffmpeg_process == None):
+            ffmpeg_command = [
+                'ffmpeg',
+                '-y',
+                '-loglevel','quiet',
+                '-r', str(self.frame_rate),
+                '-i', 'pipe:0',
+                '-preset', 'fast',
+                '-crf', '21',
+                # '-c:v', 'h264_nvenc',
+                '-pix_fmt', 'yuv420p',
+                
+                self.video_file_name
+            ]
+            self.ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE)
+        if(src_path.endswith(".aac")):
+            if(self.ffmpeg_process!= None):
+                self.ffmpeg_process.stdin.close()
+                self.ffmpeg_process.wait()
+
+            ffmpeg_command = [
+                'ffmpeg',
+                '-y',
+                '-i', self.video_file_name,
+                '-i', event.src_path,
+                '-c:v', 'copy',
+                '-c:a', 'copy',
+                
+                self.file_name
+            ]
+            print("merging...")
+            subprocess.Popen(ffmpeg_command)
+
+        try:
+            if src_path.endswith(".bmp"):
+                with open(event.src_path, 'rb') as video_file:
+                    self.ffmpeg_process.stdin.write(video_file.read())
+                    print(f"read: {event.src_path}")
+                os.remove(event.src_path)
+        except:
+            return
+def main():
+
+    folder_to_watch = "./"  # Change this to the directory you want to watch
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, folder_to_watch, recursive=True)
+    observer.start()
+    try:
+        print(f"Watching directory: {folder_to_watch}")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        if(event_handler.ffmpeg_process!=None):
+            event_handler.ffmpeg_process.stdin.close()
+    observer.join()
+
+main()
